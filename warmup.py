64d45510463e2c39fcc993ffd2572a420977f441
Ofer Koren <oferko@il.ibm.com>
2012-11-07 13:29:14 +0200

    /warmup resets cache on dev_server

--- warmup.py
+++ warmup.py
@@ -3,10 +3,18 @@ import logging
 import request_handler
 import library
 from badges import util_badges
+from app import App
 
 class Warmup(request_handler.RequestHandler):
 
     def get(self):
+        if App.is_dev_server:
+            logging.info("Warmup: deleting caches")
+            from google.appengine.ext import blobstore
+            from google.appengine.ext import db
+            for k in blobstore.BlobInfo.all():
+                logging.info(" -- %s", k.key())
+                k.delete()
 
         logging.info("Warmup: loading homepage html")
         library.library_content_html()
