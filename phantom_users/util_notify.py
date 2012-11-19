b97e94d4027d82ca859f731c1c9bf8fcb1f545c6
Ofer Koren <oferko@il.ibm.com>
2012-11-19 03:32:03 +0200

    translations

--- phantom_users/util_notify.py
+++ phantom_users/util_notify.py
@@ -1,3 +1,4 @@
+# coding=utf8
 import models
 from notifications import UserNotifier
 import request_handler
@@ -5,7 +6,7 @@ import request_handler
 def welcome(user_data):
     if user_data == None:
         return False
-    UserNotifier.push_login_for_user_data(user_data,"Welcome to Khan Academy! To get the best experience you should [login]")
+    UserNotifier.push_login_for_user_data(user_data,"ברוכים הבאים לאקדמיית קהאן! לחוויה מלאה ממולץ [login]")
     
 def update(user_data, user_exercise, threshold=False, isProf=False, gotBadge=False):
     if user_data == None:
@@ -26,28 +27,28 @@ def update(user_data, user_exercise, threshold=False, isProf=False, gotBadge=Fal
 
     # First question
     if (numquest == 1):
-        UserNotifier.push_login_for_user_data(user_data,"You've answered your first question! You should [login]")  
+        UserNotifier.push_login_for_user_data(user_data,"פתרתם את התרגיל הראשון שלכם! כדאי לכם [login]")  
     # Every 10 questions, more than 20 every 5  
     if (numquest != None and numquest % 10 == 0) or \
        (numquest != None and numquest > 20 and numquest % 5 == 0):
-        UserNotifier.push_login_for_user_data(user_data,"You've answered "+str(numquest)+" questions! You should [login]")
+        UserNotifier.push_login_for_user_data(user_data,"פתרתם "+str(numquest)+" תרגילים! כדאי לכם [login]")
     #Proficiency
     if isProf:
-        UserNotifier.push_login_for_user_data(user_data,"You're proficient in "+str(prof)+". You should [login]")
+        UserNotifier.push_login_for_user_data(user_data,"אתם מיומנים ב-"+str(prof)+". כדאי לכם [login]")
     #First Badge
     if numbadge != None and len(numbadge) == 1 and gotBadge:
         achievements_url = "%s/achievements" % user_data.profile_root
         UserNotifier.push_login_for_user_data(
                 user_data,
-                "Congrats on your first <a href='%s'>badge</a>! You should [login]" %
+                "ברכות על ה<a href='%s'>תג</a> הראשון שלכם! כדאי לכם [login]" %
                         achievements_url)
     #Every badge after
     if numbadge != None and len(numbadge) > 1 and gotBadge:
-        UserNotifier.push_login_for_user_data(user_data,"You've earned <a href='/profile'>"+str(len(numbadge))+" badges</a> so far. You should [login]")
+        UserNotifier.push_login_for_user_data(user_data,"השגתם <a href='/profile'>"+str(len(numbadge))+" תגים</a> עד כה. כדאי לכם [login]")
     #Every 2.5k points
     if numpoint != None and threshold:
         numpoint = 2500*(numpoint/2500)+2500
-        UserNotifier.push_login_for_user_data(user_data,"You've earned over <a href='/profile'>"+str(numpoint)+ " points</a>! You should [login]")
+        UserNotifier.push_login_for_user_data(user_data,"השגתם <a href='/profile'>"+str(numpoint)+ " נקודות</a>! כדאי לכם [login]")
 
 
 #Toggle Notify allows the user to close the notification bar (by deleting the memcache) until a new notification occurs. 
