66ee2e3e68dd30d39a33961f0dd16bf602abcdca
Ofer Koren <oferko@il.ibm.com>
2012-11-23 03:15:04 +0200

    utils for translating python files

--- hebkhan_scripts/utils.py
+++ hebkhan_scripts/utils.py
@@ -4,7 +4,6 @@ Created on Nov 4, 2012
 @author: oferko
 '''
 
-from BeautifulSoup import BeautifulStoneSoup, NavigableString
 from path3 import path
 import re
 import hashlib
@@ -12,17 +11,91 @@ import urllib2
 import csv
 import itertools
 import sys
+import ast
 
 APP_ROOT = path(__file__).parent.parent
 
-def get_htmls():
-    for tmpl in (APP_ROOT + "/templates").walkfiles("*.html"):
-        yield tmpl
+class TranslatableFile(object):
+    HEADER = ""
 
-    yield APP_ROOT + "/khan-exercises/exercises/khan-exercise.html"
+    def __init__(self, f):
+        self.relative_path = APP_ROOT.relpathto(f)
+        self.file_digest = hashlib.md5(self.relative_path).hexdigest()[:DIGEST_LEN]
+        check_collision(self.file_digest)
+        self.path = f
+        self.orig = f + ".orig"
+        source = self.orig if self.orig.isfile() else f
+        with open(source) as opened:
+            self.text = opened.read()
+    def __repr__(self):
+        return "%s(%s - %s)" % (self.__class__.__name__, self.file_digest, self.relative_path)
 
-    for tmpl in (APP_ROOT + "/clienttemplates").walkfiles("*.handlebars"):
-        yield tmpl
+    def preprocess(self, translation):
+        return translation
+
+    def translate(self, translations):
+        if not self.orig.isfile():
+            with open(self.orig, "w") as f:
+                f.write(self.text)
+            print self.relative_path, "->", self.orig
+        translated_text = self.HEADER
+        cursor = 0
+        for orig_text, (st, end), line_no, text_digest in self.iter_translatables():
+            translated_text += self.text[cursor:st]
+            translation = self.preprocess(translations.get(text_digest))
+            chunk = self.text[st:end]
+            translated_text += chunk.replace(orig_text.strip(), translation) if translation is not None else chunk
+            cursor = end
+        translated_text += self.text[cursor:]
+
+        with open(self.path, "w") as f:
+            f.write(translated_text)
+            print "%s: %s" % (self.path, len(translations))
+
+    @classmethod
+    def get_files(cls):
+        for f in cls._get_files():
+            yield cls(f)
+
+class TranslatablePython(TranslatableFile, ast.NodeVisitor):
+    HEADER = "# coding=utf8\n\n"
+    @classmethod
+    def _get_files(cls):
+        for tmpl in (APP_ROOT + "/badges").walkfiles("*badge*.py"):
+            yield tmpl
+
+    def iter_translatables(self):
+        self.lines = self.text.splitlines()
+        self.line_breaks = [None,0] + [m.start() for m in RE_LINEBREAK.finditer(self.text+"\n")]
+        self.strings = []
+        tree = ast.parse(self.text, self.relative_path)
+        self.visit(tree)
+        for line_no, span, string in self.strings:
+            line = self.lines[line_no-1]
+            nice_text = line[line.find(string)-1:].strip()
+            if len(set(nice_text.replace(string, ""))) == 1:
+                # there's nothing but quotes around our string
+                nice_text = string
+            line = line.strip()
+            if not (line.startswith("return") or "self.description" in line):
+                continue
+            digest = hashlib.md5(nice_text).hexdigest()[:DIGEST_LEN]
+            yield nice_text, span, line_no, digest
+
+    def preprocess(self, translation):
+        if not translation:
+            return
+        return ("u" + translation
+                if translation.startswith("\"")
+                else ('"+u"%s' % translation)
+                )
+
+    def visit_Str(self, node):
+        if node.s:
+            st = self.line_breaks[node.lineno] + node.col_offset
+            end = self.line_breaks[node.lineno+1]
+            self.strings.append((node.lineno, (st, end), node.s))
+
 
 NON_TEXT = set("[document] script style".split())
 
@@ -43,62 +116,75 @@ RE_ATTR_TAGS = {'input'     : re.compile("""(value|placeholder)=(?P<qt>\"|')(.+?
                 'img'       : re.compile("""(title)=(?P<qt>\"|')(.+?)(?P=qt)"""),
                 }
 RE_LINEBREAK = re.compile("\n")
-def iter_translateables(html):
 
-    def iterator():
-        line_breaks = [m.start() for m in RE_LINEBREAK.finditer(html+"\n")]
-        line_no = 1
-        cursor = 0
-        current_tag = None
-        matches = RE_TAGS.finditer(html)
-        for match in matches:
-            ended, tag, attrs, end = match.groups()
-            tag_start, tag_end = match.span()
-            if tag and not ended:
-                if not end:
-                    current_tag = tag
-                regex = RE_ATTR_TAGS.get(tag)
-                if regex:
-                    attrs_start = tag_start + html[tag_start:tag_end].find(attrs)
-                    for match in regex.finditer(attrs):
-                        name, qt, value = match.groups()
-                        attr_start, attr_end = match.start(), match.end()
-                        while attrs_start+attr_start > line_breaks[line_no-1]:
-                            line_no += 1
-                        if value == "Ask a question about this video":
-                            pass
-                        yield value, (attrs_start+attr_start, attrs_start+attr_end), line_no
+class TranslatableHTML(TranslatableFile):
 
+    @classmethod
+    def _get_files(cls):
+        for tmpl in (APP_ROOT + "/templates").walkfiles("*.html"):
+            yield tmpl
 
-            if current_tag in NON_TEXT:
-                for match in matches:
-                    ended, tag, _, _ = match.groups()
-                    if tag == current_tag:
-                        assert ended, match.group(0)
-                        tag_end = match.end()
-                        current_tag = None
-                        break
+        yield APP_ROOT + "/khan-exercises/exercises/khan-exercise.html"
+
+        for tmpl in (APP_ROOT + "/clienttemplates").walkfiles("*.handlebars"):
+            yield tmpl
+
+    def iter_translatables(self):
+        html = self.text
+        def iterator():
+            line_breaks = [m.start() for m in RE_LINEBREAK.finditer(html+"\n")]
+            line_no = 1
+            cursor = 0
+            current_tag = None
+            matches = RE_TAGS.finditer(html)
+            for match in matches:
+                ended, tag, attrs, end = match.groups()
+                tag_start, tag_end = match.span()
+                if tag and not ended:
+                    if not end:
+                        current_tag = tag
+                    regex = RE_ATTR_TAGS.get(tag)
+                    if regex:
+                        attrs_start = tag_start + html[tag_start:tag_end].find(attrs)
+                        for match in regex.finditer(attrs):
+                            name, qt, value = match.groups()
+                            attr_start, attr_end = match.start(), match.end()
+                            while attrs_start+attr_start > line_breaks[line_no-1]:
+                                line_no += 1
+                            if value == "Ask a question about this video":
+                                pass
+                            yield value, (attrs_start+attr_start, attrs_start+attr_end), line_no
+    
+    
+                if current_tag in NON_TEXT:
+                    for match in matches:
+                        ended, tag, _, _ = match.groups()
+                        if tag == current_tag:
+                            assert ended, match.group(0)
+                            tag_end = match.end()
+                            current_tag = None
+                            break
+                    else:
+                        raise Exception("Could not close %s" % current_tag)
                 else:
-                    raise Exception("Could not close %s" % current_tag)
-            else:
-                while cursor > line_breaks[line_no-1]:
-                    line_no += 1
-                yield html[cursor:tag_start], (cursor, tag_start), line_no
-
-            cursor = tag_end
-
-        tag_start = len(html)
-        while cursor > line_breaks[line_no-1]:
-            line_no += 1
-        yield html[cursor:tag_start], (cursor, tag_start), line_no
-
-    for text, span, line_no in iterator():
-        if text:
-            nice_text = text.strip()
-            if not is_translateable(nice_text):
-                continue
-            digest = hashlib.md5(nice_text).hexdigest()[:DIGEST_LEN]
-            yield nice_text, span, line_no, digest
+                    while cursor > line_breaks[line_no-1]:
+                        line_no += 1
+                    yield html[cursor:tag_start], (cursor, tag_start), line_no
+    
+                cursor = tag_end
+    
+            tag_start = len(html)
+            while cursor > line_breaks[line_no-1]:
+                line_no += 1
+            yield html[cursor:tag_start], (cursor, tag_start), line_no
+    
+        for text, span, line_no in iterator():
+            if text:
+                nice_text = text.strip()
+                if not is_translateable(nice_text):
+                    continue
+                digest = hashlib.md5(nice_text).hexdigest()[:DIGEST_LEN]
+                yield nice_text, span, line_no, digest
 
 def is_translateable(nice_text):
     if not nice_text:
@@ -119,14 +205,12 @@ def check_collision(digest, _digests=set()):
         raise Exception("Collision!")
     _digests.add(digest)
 
-def iter_htmls():
-    htmls = list(get_htmls())
-    for i, f in enumerate(htmls):
-        relative_path = APP_ROOT.relpathto(f)
-        file_digest = hashlib.md5(relative_path).hexdigest()[:DIGEST_LEN]
-        check_collision(file_digest)
-        #print i, file_digest, relative_path
-        yield f, file_digest, relative_path
+
+def iter_files():
+    return itertools.chain(
+                           TranslatableHTML.get_files(),
+                           TranslatablePython.get_files(),
+                           )
 
 def extract_for_translation():
     data = get_translated_data(all=True)
@@ -135,30 +219,25 @@ def extract_for_translation():
     new_items = []
     with open("extracted_text3.tab","w") as output:
         print >>output, "\t".join("id,file digest,line no,text digest,text,translation".split(","))
-        for f, file_digest, relative_path in iter_htmls():
-            if f.endswith("editexercise.html"):
-                pass
-            orig = f + ".orig"
-            orig = orig if orig.isfile() else f
-            orig_html = open(orig).read()
-            translations = data.get(file_digest) or {}
+        for f in iter_files():
+            translations = data.get(f.file_digest) or {}
             if translations == "IGNORE":
                 print "SKIPPING:", f
-                print >>output, "%s\t'%s\t%s\t%s\t%s\tIGNORE" % (counter(), file_digest, "", "", relative_path)
+                print >>output, "%s\t'%s\t%s\t%s\t%s\tIGNORE" % (counter(), f.file_digest, "", "", f.relative_path)
                 print >>output, counter()
                 continue
 
             lines = []
-            for text, _, line_no, text_digest in iter_translateables(orig_html):
+            for text, _, line_no, text_digest in f.iter_translatables():
                 translation = translations.get(text_digest, "")
                 if translation == "NOOP":
                     translation = ""
-                lines.append(("'"+file_digest, "#%s" % line_no, "'"+text_digest, "'%s" % text, "'%s" % translation))
+                lines.append(("'"+f.file_digest, "#%s" % line_no, "'"+text_digest, "'%s" % text, "'%s" % translation))
                 if text_digest not in translations:
                     new_items.append(lines[-1])
 
             if lines:
-                print >>output, "\t".join((counter(), "'" + file_digest, "", "", relative_path))
+                print >>output, "\t".join((counter(), "'" + f.file_digest, "", "", f.relative_path))
                 for line in lines:
                     print >>output, "\t".join((counter(),) + line)
                 print >>output, counter()
@@ -194,61 +273,11 @@ def import_translation():
 
     data = get_translated_data()
 
-    for html_file, file_digest, _pth in iter_htmls():
-        translations = data.get(file_digest)
+    for f in iter_files():
+        translations = data.get(f.file_digest)
         if not translations or translations == "IGNORE":
             continue
-
-        orig = html_file + ".orig"
-        if not orig.isfile():
-            html_file.copyfile(orig)
-            print html_file, "->", orig
-        orig_html = open(orig).read()
-        translated_html = ""
-        cursor = 0
-        for orig_text, (st, end), line_no, text_digest in iter_translateables(orig_html):
-            translated_html += orig_html[cursor:st]
-            translation = translations.get(text_digest)
-            chunk = orig_html[st:end]
-            translated_html += chunk.replace(orig_text.strip(), translation) if translation is not None else chunk
-            cursor = end
-        translated_html += orig_html[cursor:]
-
-        with open(html_file, "w") as f:
-            f.write(translated_html)
-            print "%s: %s" % (html_file, len(translations))
+        f.translate(translations)
 
 
-
-#===============================================================================
-# 
-#===============================================================================
-
-def get_soup(html_file):
-    with open(html_file) as f:
-        html = f.read()
-
-    html = RE_TEMPLATE.sub(simple_escape, html)
-    return BeautifulStoneSoup(html)
-
-def iter_translateables_soup(soup):
-    for tag in soup.findAll(text=True):
-        if tag.parent.name in NON_TEXT:
-            continue
-        parents = list(reversed([t.name for t in tag.parentGenerator() if t][:-1]))
-        if "script" in parents:
-            continue
-        parents = ".".join(parents)
-        for i, line in enumerate(tag.string.splitlines()):
-            nice_text = line.strip()
-            if not is_translateable(nice_text):
-                continue
-            digest = hashlib.md5(nice_text).hexdigest()[:DIGEST_LEN]
-            yield (tag, i), "%s[%s]"%(parents, i), digest, nice_text
-
-    for tag in soup.findAll(attrs=dict(placeholder=True)):
-        placeholder = tag.attrMap['placeholder']
-        parents = ".".join(reversed([t.name for t in tag.parentGenerator() if t][:-1]))
-        digest = hashlib.md5(placeholder).hexdigest()[:DIGEST_LEN]
-        yield (tag, 'placeholder'), "%s[placeholder]" % parents, digest, placeholder
 
