971f35a43d64bd68dae01fdd47e2fee020c665db
Shay Erlichmen <erlichmen@gmail.com>
2013-11-08 09:41:05 +0200

    add the missing part for appestat

--- app/appengine_config.py
+++ app/appengine_config.py
@@ -8,3 +8,8 @@
 remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = (
     'HTTP_X_APPENGINE_INBOUND_APPID', ['khanexercises'])
 
+
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
