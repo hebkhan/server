dbf5d96123a86b79e8e6ac901a6731fb15a8b7a2
Ofer Koren <ofer@weka.io>
2014-10-30 18:36:56 +0200

    unicode bug fix in notifying phantom users

--- app/phantom_users/util_notify.py
+++ app/phantom_users/util_notify.py
@@ -19,7 +19,7 @@ def update(user_data, user_exercise, threshold=False, isProf=False, gotBadge=Fal
 
     if user_exercise != None:
         numquest = user_exercise.total_done
-        prof = str(models.Exercise.get_by_name(user_exercise.exercise).display_name)
+        prof = models.Exercise.get_by_name(user_exercise.exercise).display_name
 
 
     numbadge = user_data.badges
@@ -31,10 +31,10 @@ def update(user_data, user_exercise, threshold=False, isProf=False, gotBadge=Fal
     # Every 10 questions, more than 20 every 5  
     if (numquest != None and numquest % 10 == 0) or \
        (numquest != None and numquest > 20 and numquest % 5 == 0):
-        UserNotifier.push_login_for_user_data(user_data,"פתרתם "+str(numquest)+" תרגילים! כדאי לכם [login]")
+        UserNotifier.push_login_for_user_data(user_data,"פתרתם %s תרגילים! כדאי לכם [login]" % numquest)
     #Proficiency
     if isProf:
-        UserNotifier.push_login_for_user_data(user_data,"אתם מיומנים ב-"+str(prof)+". כדאי לכם [login]")
+        UserNotifier.push_login_for_user_data(user_data,"אתם מיומנים ב-%s. כדאי לכם [login]" % prof)
     #First Badge
     if numbadge != None and len(numbadge) == 1 and gotBadge:
         achievements_url = "%s/achievements" % user_data.profile_root
